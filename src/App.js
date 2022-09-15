78ac85e0169ec108ade3e914411da67890de079f
hoanghuy1801 <hoanghuy.pham1801@gmail.com>
2022-09-15 22:05:48 +0700

    Design FE

--- src/App.js
+++ src/App.js
@@ -1,11 +1,198 @@
-
-
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
+    <div className="hd-master">
+      <nav className='navbar app'>App Bar</nav>
+      <nav className='navbar board'>Board Bar</nav>
+      <nav className='board-columns'>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://cdn.tgdd.vn/2022/09/campaign/14pro-tim-600x600-7.png' alt='hoanghuydev-alt-img'></img>
+              Title:Hoang Huy
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
 
-      <h1 className="hello-world">  Hello world</h1>
+
+
+          </ul>
+          <footer>Add anther card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://cdn.tgdd.vn/2022/09/campaign/14pro-tim-600x600-7.png' alt='hoanghuydev-alt-img'></img>
+              Title:Hoang Huy
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+
+          </ul>
+          <footer>Add anther card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://cdn.tgdd.vn/2022/09/campaign/14pro-tim-600x600-7.png' alt='hoanghuydev-alt-img'></img>
+              Title:Hoang Huy
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+
+
+
+          </ul>
+          <footer>Add anther card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://cdn.tgdd.vn/2022/09/campaign/14pro-tim-600x600-7.png' alt='hoanghuydev-alt-img'></img>
+              Title:Hoang Huy
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+
+
+
+          </ul>
+          <footer>Add anther card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://cdn.tgdd.vn/2022/09/campaign/14pro-tim-600x600-7.png' alt='hoanghuydev-alt-img'></img>
+              Title:Hoang Huy
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+
+
+
+          </ul>
+          <footer>Add anther card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://cdn.tgdd.vn/2022/09/campaign/14pro-tim-600x600-7.png' alt='hoanghuydev-alt-img'></img>
+              Title:Hoang Huy
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+
+
+
+          </ul>
+          <footer>Add anther card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://cdn.tgdd.vn/2022/09/campaign/14pro-tim-600x600-7.png' alt='hoanghuydev-alt-img'></img>
+              Title:Hoang Huy
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+
+
+
+          </ul>
+          <footer>Add anther card</footer>
+        </div>
+      </nav>
     </div>
   );
 }
